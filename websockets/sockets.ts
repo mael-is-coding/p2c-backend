ddf1a371b9fe071bcacf92d6283e708da83f4073
mael-is-coding <maelsalem@outlook.com>
2025-12-11 17:03:09 +0100

    feat: working on websockets - server completed, client/* concept

--- websockets/sockets.ts
+++ websockets/sockets.ts
@@ -1,35 +1,37 @@
 
 Deno.serve({
     port: 8080,
-    hostname: "127.0.0.1",
-    async handler(request: Request) {
-        if(request.headers.get("upgrade") !== "websocket") {
-            const file = await Deno.open("./ws/index.html");
+    hostname: "0.0.0.0",
+    handler(request: Request) {
+        console.log("handler runs");
 
-            return new Response(file.readable);
+        const UsrSockets = new Map<number, WebSocket>();
+
+        if(request.headers.get("upgrade") !== "websocket") {
+            return new Response(null, { status: 426} );
         }
 
         const { socket, response } = Deno.upgradeWebSocket(request);
 
-        socket.onopen = () => {
-            console.log("OPEN")
+        const Str_uid = new URL(`${request.url}`).searchParams.get("id");
+
+        if(Str_uid) {
+            const uid = parseInt(Str_uid);
+            UsrSockets.set(uid, socket);
+        }
+
+        socket.onclose = (close_ev) => {
+            console.log("close_ev : " + close_ev.reason);
         }
 
         socket.onmessage = (event) => {
-            console.log(`
-                RECEIVED : ${event.data}
-            `);
-            socket.send("pong");
+            console.log(`received a message : ${event.data}`);
         }
 
-        socket.onclose = () => {
-            console.log("DISCONNECTED");
+        socket.onclose = (event) => {
+            console.log("he's outta heare")
         }
 
-        socket.onerror = (error) => {
-            console.log(`ERROR FROM SERVER : ${error}`)
-        }
-
-        return response
-    }
+        return response;
+    },
 });
